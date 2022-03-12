08c114de3588831aebc46e9dd2ac8e44b05b8239
EuigunJung <63629484+EuigunJung@users.noreply.github.com>
2022-03-11 19:57:20 -0700

    Add project files.

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission10.Models;
+
+namespace Mission10.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket repo;
+
+        public CartSummaryViewComponent(Basket temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //Displaying the cart summary
+            return View(repo);
+        }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
+namespace Mission10.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext()
+        {
+        }
+
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public  DbSet<Book> Books { get; set; }
+        public  DbSet<Checkout> Checkouts { get; set; }
+      
+    }
+}
